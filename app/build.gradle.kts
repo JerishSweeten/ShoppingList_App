a83dda9c7d5e6bde90fad0851e56ab80948037a5
JerishSweeten <jerishsweeten@gmail.com>
2026-07-15 15:13:40 +0530

    Added Button and LazyColumn

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 android {
     namespace = "com.excel.shopping"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
+        version = release(37) {
+            minorApiLevel = 0
         }
     }
 
--- app/src/main/java/com/excel/shopping/MainActivity.kt
+++ app/src/main/java/com/excel/shopping/MainActivity.kt
@@ -4,14 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.excel.shopping.ui.theme.ShoppingTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import kotlin.collections.listOf
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +35,46 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ShoppingTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                shopy()
+
                 }
+
             }
         }
     }
-}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun shopy() {
+    var Shopitems by remember { mutableStateOf(listOf<String>()) }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement =Arrangement.Center)
+
+    {   Spacer(modifier = Modifier.height(18.dp))
+        Button(
+            onClick = {},
+            colors = buttonColors(containerColor = Color.Cyan, contentColor = Color.White),
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text("Add Item")
+        }
+        LazyColumn(modifier = Modifier.fillMaxSize().
+        padding(16.dp)
+        ) {
+            items(){
+
+            }
+        }
+    }
+
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ShoppingTheme {
-        Greeting("Android")
+        shopy()
     }
 }
